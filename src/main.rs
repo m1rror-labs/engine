901dcd260471303a526adfbe5d701f749d4d2fc6
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2025-02-01 16:09:44 -0700

    fix build

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,6 @@ async fn main() -> std::io::Result<()> {
 
     let deps = Dependencies::new(svm);
 
-    let ip = env::var("SERVER_IP").unwrap_or_else(|_| "0.0.0.0".to_string());
-    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(deps.clone())) // Share dependencies
