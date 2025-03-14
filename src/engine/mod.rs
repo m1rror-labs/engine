1ea05dc300074d2273388a5f6ae1246f257865fc
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2025-03-14 15:41:24 +0100

    speed things up more

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -259,7 +259,7 @@ impl<T: Storage + Clone + 'static> SVM<T> for SvmEngine<T> {
                         break;
                     }
                 };
-                if next_block_read.block_height > initial_slot + 50 {
+                if next_block_read.block_height > initial_slot + 5 {
                     match tx.send(None).await {
                         Ok(_) => {}
                         Err(_) => {}
